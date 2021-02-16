92d2dcd72dd8271c234f1575e0175b05a3b334ca
Kenny Boyer <ksboyerii@gmail.com>
2021-02-16 17:53:20 -0600

    changed generatepassword to writepassword on ln 43

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
+//Add an array
 var generateBtn = document.querySelector("#generate");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()";
+var password ="";
+var passwordLength = parseInt(document.generator.length.value);
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +14,31 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  var allowed = '';
+    if (document.generator.uppercase.checked) {
+      allowed = allowed + uppercase;
+    }
+    if (document.generator.lowercase.checked) {
+      allowed = allowed + lowercase;
+    }
+    if (document.generator.numbers.checked) {
+      allowed = allowed + numbers;
+    }
+    if (document.generator.symbols.checked) {
+      allowed = allowed + symbols;
+    }
+  }
+
+
+
+for(var i=0; i <length; i++) {
+  var random = Math.floor(Math.random() * allowed.length);
+  password += allowed[random];
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
