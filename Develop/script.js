08925614d92379ca265113a150dd16a2fa76f6e8
Kenny Boyer <ksboyerii@gmail.com>
2021-02-16 22:07:37 -0600

    fixed randomizer and password generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,48 @@
 // Assignment Code
-//Add an array
 var generateBtn = document.querySelector("#generate");
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*()";
 var password ="";
-var passwordLength = parseInt(document.generator.length.value);
+var passwordLength = parseInt(document.generator.pwlength.value);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
-
+//functions for the difference variable that can be in the password
 function generatePassword() {
+  password = "";
   var allowed = '';
     if (document.generator.uppercase.checked) {
-      allowed = allowed + uppercase;
+      allowed += uppercase;
     }
     if (document.generator.lowercase.checked) {
-      allowed = allowed + lowercase;
+      allowed += lowercase;
     }
     if (document.generator.numbers.checked) {
-      allowed = allowed + numbers;
+      allowed += numbers;
     }
     if (document.generator.symbols.checked) {
-      allowed = allowed + symbols;
+      allowed += symbols;
     }
+    console.log(allowed.length)
+    //randomizing math function
+    for(var i = 0; i < passwordLength; i++) {
+      var random = Math.floor(Math.random() * allowed.length);
+      console.log(random)
+     password += allowed[random];
+    }
+    
+  
   }
 
 
 
-for(var i=0; i <length; i++) {
-  var random = Math.floor(Math.random() * allowed.length);
-  password += allowed[random];
-}
 
-// Add event listener to generate button
+// Event Listener Function
 generateBtn.addEventListener("click", writePassword);
 
