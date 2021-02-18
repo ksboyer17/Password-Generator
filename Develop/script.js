464afa226949f0d48b053d18996e11e7e2bb50d5
Kenny Boyer <ksboyerii@gmail.com>
2021-02-17 18:53:53 -0600

    corrected password length function and moved password elements  inside the main box and set alerts for when the correct selections are not made

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,11 @@
-// Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*()";
 var password ="";
-var passwordLength = parseInt(document.generator.pwlength.value);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,34 +13,40 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-//functions for the difference variable that can be in the password
+
+//functions for the different variable that can be in the password
+//length parameters
 function generatePassword() {
   password = "";
   var allowed = '';
-    if (document.generator.uppercase.checked) {
-      allowed += uppercase;
+  var passwordLength = parseInt(document.generator.pwlength.value);
+      if (passwordLength >= 8 && passwordLength <= 128) {
+      if (!document.generator.uppercase.checked && !document.generator.lowercase.checked && !document.generator.numbers.checked && !document.generator.symbols.checked) {
+        alert("Please check a box to continue!")
+      } else{
+      if (document.generator.uppercase.checked) {
+        allowed += uppercase;
+      }
+      if (document.generator.lowercase.checked) {
+        allowed += lowercase;
+      }
+      if (document.generator.numbers.checked) {
+        allowed += numbers;
+      }
+      if (document.generator.symbols.checked) {
+        allowed += symbols;
+      }
+      console.log(allowed.length)
+      //randomizing math function
+      for(var i = 0; i < passwordLength; i++) {
+        var random = Math.floor(Math.random() * allowed.length);
+        console.log(random)
+      password += allowed[random];
+      }}
+    } else{
+      alert("Please enter a value between 8 and 128!")
     }
-    if (document.generator.lowercase.checked) {
-      allowed += lowercase;
-    }
-    if (document.generator.numbers.checked) {
-      allowed += numbers;
-    }
-    if (document.generator.symbols.checked) {
-      allowed += symbols;
-    }
-    console.log(allowed.length)
-    //randomizing math function
-    for(var i = 0; i < passwordLength; i++) {
-      var random = Math.floor(Math.random() * allowed.length);
-      console.log(random)
-     password += allowed[random];
-    }
-    
-  
   }
-
-
 
 
 // Event Listener Function
